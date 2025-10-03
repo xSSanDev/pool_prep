9a1869606ed835b78c006a41eec5695d0f6c12d6
xSSanDev <xssanass@gmail.com>
2025-10-03 12:20:33 +0200

    ex05

--- C00/ ft_print_comb.c
+++ C00/ ft_print_comb.c
@@ -5,20 +5,20 @@ void    ft_putchar(char d)
 }
 void ft_print_comb(void)
 {
-    char first_digit; //1
-    char second_digit; //2
-    char third_digit; //3
+    char first_digit;
+    char second_digit;
+    char third_digit;
 
-    first_digit = '0'; // set first_digit to 0
-    while (first_digit <= '9') //the condition is True
+    first_digit = '0';
+    while (first_digit <= '9')
     {
-        second_digit = '0';  // set second_digit to 0
-        while (second_digit <= '9') // the condition is True
+        second_digit = '0';
+        while (second_digit <= '9')
         {
-            third_digit = '0'; //set the third_digit to 3
-            while (third_digit <= '9') // the condition is true
+            third_digit = '0';
+            while (third_digit <= '9')
             {
-                if (first_digit < second_digit && second_digit < third_digit)// it will skip the if block cause the condition is False
+                if (first_digit < second_digit && second_digit < third_digit)
                 {
                     ft_putchar(first_digit);
                     ft_putchar(second_digit);
@@ -29,14 +29,14 @@ void ft_print_comb(void)
                         ft_putchar(' ');
                     }
                 }
-                third_digit++; // third_digit will become 1 the 10 the the leave the inner loop
+                third_digit++;
             }
-            second_digit++; // first it become 1
+            second_digit++;
         }
         first_digit++;
     }
 }
-int main() // the compiler start execting the code from the int main
+int main()
 {
     ft_print_comb();
     return (0);
