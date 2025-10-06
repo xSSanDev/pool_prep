3daacb8fe6ccbfde5edd65ad45811af8244c9bc6
xSSanDev <xssanass@gmail.com>
2025-10-06 12:53:40 +0200

    ex01

--- C01/ft_ultimate_ft.c
+++ C01/ft_ultimate_ft.c
@@ -1,9 +1,41 @@
 #include <unistd.h>
-void    ft_ulttimate_ft(int *******nbr)
+void    ft_putchar(char c)
 {
-
+        write(1, &c, 1);
+}
+void    ft_ultimate_ft(int *********nbr)
+{
+        *********nbr = 42;
+}
+void    ft_putnbr(int nb)
+{
+        if(nb < 0)
+        {
+                ft_putchar('-');
+                nb =- nb;
+        }
+        if(nb >= 10)
+        {
+                ft_putnbr(nb / 10);
+        }
+        ft_putchar((nb % 10) + '0');
 }
 int main()
 {
-  return (0);
+        int my_number ;
+        int *ptr1 = &my_number;
+        int **ptr2 = &ptr1;
+        int ***ptr3 = &ptr2;
+        int ****ptr4 = &ptr3;
+        int *****ptr5 = &ptr4;
+        int ******ptr6 = &ptr5;
+        int *******ptr7 = &ptr6;
+        int ********ptr8 = &ptr7;
+        int *********ptr9 = &ptr8;
+        my_number = 787;
+        ft_putnbr(my_number);
+        ft_putchar('\n');
+        ft_ultimate_ft(ptr9);
+        ft_putnbr(my_number);
+        return(0);
 }
