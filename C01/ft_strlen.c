dbf52dfb5248efa6dd447c330d1bf3c4774d47ae
xSSanDev <xssanass@gmail.com>
2025-10-06 15:40:44 +0200

    ex05

--- C01/ft_strlen.c
+++ C01/ft_strlen.c
@@ -1,1 +1,15 @@
 #include <unistd.h>
+int     ft_strlen(char *str)
+{
+    int i = 0;
+    while(str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+int main()
+{
+    ft_strlen("zanass");
+    return(0);
+}
