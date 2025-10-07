0df95726b8c36208f69a8f2d9d3bad624a539ce0
xSSanDev <xssanass@gmail.com>
2025-10-07 11:08:41 +0200

    ex06

--- C01/ft_strlen.c
+++ C01/ft_strlen.c
@@ -1,4 +1,21 @@
 #include <unistd.h>
+void    ft_putchar(char c)
+{
+    write(1,&c,1);
+}
+void    ft_putnbr(int nb)
+{
+    if(nb < 0)
+    {
+        ft_putchar('-');
+        nb =- nb;
+    }
+    if(nb >= 10)
+    {
+        ft_putnbr(nb / 10);
+    }
+    ft_putchar((nb % 10) + '0');
+}
 int     ft_strlen(char *str)
 {
     int i = 0;
@@ -8,8 +25,9 @@ int     ft_strlen(char *str)
     }
     return i;
 }
-int main()
-{
-    ft_strlen("zanass");
+int main() {
+    int result;
+    result = ft_strlen("zanassdsdc");
+    ft_putnbr(result);
     return(0);
 }
