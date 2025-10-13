af4bc02d6a78ddf0f91b1d1d22ed287d1d89ec0a
xSSanDev <xssanass@gmail.com>
2025-10-13 15:53:32 +0200

    ex03

--- C02/ft_str_is_numeric.c
+++ C02/ft_str_is_numeric.c
@@ -1,5 +1,38 @@
 #include <unistd.h>
-void    ft_str_is_numeric(char *str)
+void    ft_putchar(char c)
 {
-
+    write(1,&c,1);
+}
+void    ft_putnbr(int nb)
+{
+    if(nb < 0)
+    {
+        ft_putchar('-');
+        nb =- nb;
+    }
+    if(nb >= 10)
+    {
+        ft_putnbr(nb / 10);
+    }
+    ft_putchar((nb % 10) + '0');
+}
+int     ft_str_is_numeric(char *str)
+{
+    int i = 0;
+    while(str[i] != '\0')
+    {
+        if(!(str[i] >= '0' && str[i] <= '9'))
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+int main()
+{
+    char a[10] = "2763523";
+    int result;
+    result = ft_str_is_numeric(a);
+    ft_putnbr(result);
 }
