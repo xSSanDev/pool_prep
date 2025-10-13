71a095eb634e818b32f92f75fc2a34ace5908c3c
xSSanDev <xssanass@gmail.com>
2025-10-13 16:11:33 +0200

    ex04

--- C02/ft_str_is_lowercase.c
+++ C02/ft_str_is_lowercase.c
@@ -1,9 +1,40 @@
 #include <unistd.h>
-int    ft_str_is_lowercase(char *str)
+void    ft_putchar(char c)
 {
-
+        write(1,&c,1);
+}
+void    ft_putnbr(int nb)
+{
+        if(nb < 0)
+        {
+                ft_putchar('-');
+                nb =- nb;
+        }
+        if(nb >= 10)
+        {
+                ft_putnbr(nb / 10);
+        }
+        ft_putchar((nb % 10) + '0');
+}
+int     ft_str_is_lowercase(char *str)
+{
+        int i = 0;
+        while(str[i] != '\0')
+        {
+                if(!(str[i] >= 'a' && str[i] <= 'z'))
+                {
+                        return 0;
+                }
+                i++;
+        }
+        return 1;
 }
 int main()
 {
-
+        char a[10] = "fhhdf6";
+        int result;
+        result = ft_str_is_lowercase(a);
+        ft_putnbr(result);
+        ft_putchar('\n');
+        return 0;
 }
