64fd8620eeaffe698f7b6c160ade0c15f38f41f7
xSSanDev <xssanass@gmail.com>
2025-10-13 16:14:05 +0200

    ex05

--- C02/ft_str_is_uppercase.c
+++ C02/ft_str_is_uppercase.c
@@ -1,14 +1,40 @@
 #include <unistd.h>
-void    putchar(char c)
+void    ft_putchar(char c)
 {
-  write(1, &c, 1);
+        write(1,&c,1);
 }
-int    ft_str_is_uppercase(char *str)
+void    ft_putnbr(int nb)
 {
-
+        if(nb < 0)
+        {
+                ft_putchar('-');
+                nb =- nb;
+        }
+        if(nb >= 10)
+        {
+                ft_putnbr(nb / 10);
+        }
+        ft_putchar((nb % 10) + '0');
+}
+int     ft_str_is_uppercase(char *str)
+{
+        int i = 0;
+        while(str[i] != '\0')
+        {
+                if(!(str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                        return 0;
+                }
+                i++;
+        }
+        return 1;
 }
 int main()
 {
-
-  return 0;
+        char a[10] = "ZANASS";
+        int result;
+        result = ft_str_is_uppercase(a);
+        ft_putnbr(result);
+        ft_putchar('\n');
+        return 0;
 }
