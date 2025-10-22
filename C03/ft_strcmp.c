b1c4fc82ae1012bc06102c21cff2f1eb0fcf1cbb
xSSanDev <xssanass@gmail.com>
2025-10-22 13:39:42 +0200

    ex00

--- C03/ft_strcmp.c
+++ C03/ft_strcmp.c
@@ -1,20 +1,37 @@
+#include <unistd.h>
+void    ft_putchar(char c)
+{
+    write(1,&c,1);
+}
+void    ft_putnbr(int nb)
+{
+    if(nb < 0)
+    {
+        ft_putchar('-');
+        nb =- nb;
+    }
+    if(nb >= 10)
+    {
+        ft_putnbr(nb / 10);
+    }
+    ft_putchar((nb % 10) + '0');
+}
 int ft_strcmp(char *s1, char *s2)
 {
     int i;
 
     i = 0;
-    // This loop does all the work. It stops at the end of s1 OR at the first difference.
     while (s1[i] != '\0' && s1[i] == s2[i])
     {
         i++;
     }
-
-    // After the loop, return the difference at the stopping point.
     return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 int main()
 {
-  ft_strcmp("heKlo", "hello");
-
-  return 0;
+    char a[10] = "He\nLlo";
+    char b[10] = "Hello";
+    ft_strcmp(a,b);
+    ft_putnbr(ft_strcmp(a,b));
+    return 0;
 }
