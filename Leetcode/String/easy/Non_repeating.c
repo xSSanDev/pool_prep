dbe8f34b73fdddec5789774d9169ffb65e620626
xSSanDev <xssanass@gmail.com>
2025-10-25 16:38:45 +0200

    String

---
+++ Leetcode/String/easy/Non_repeating.c
@@ -0,0 +1,3 @@
+/*Question 1:Non repeating character - You are given a string consisting of only lower case and upper-case English
+Alphabets and integers 0 to 9. Write a function that will take this string as Input and return the index of the first
+character that is non-repeating.*/
---
+++ Leetcode/String/easy/Palindrome.c
@@ -0,0 +1,1 @@
+/*Question 2:Palindrome-You are given a string. Write a function to check whether the string is a palindrome or not.*/
