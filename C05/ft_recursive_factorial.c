b848be2dd274303a85efb57adb5d92c4895fbf7d
xSSanDev <xssanass@gmail.com>
2025-11-03 16:55:30 +0100

    ex01

---
+++ C05/ft_recursive_factorial.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+void    ft_putchar(char c)
+{
+    write(1,&c,1);
+}
+void    ft_putnbr(int nb)
+{
+    long long n_long;
+    n_long = nb;
+
+    if(nb >= 10)
+        ft_putnbr(n_long / 10);
+    ft_putchar((n_long % 10) + '0');
+}
+int     ft_recursive_factorial(int  nb)
+{
+    // first case : invalid factorial
+    if(nb < 0)
+        return 0;
+    // base case
+    if(nb == 0)
+        return 1;
+    else
+    {
+        return (nb * ft_recursive_factorial(nb - 1));
+    }
+}
+int main()
+{
+    ft_putnbr(ft_recursive_factorial(12));
+
+    return 0;
+}
