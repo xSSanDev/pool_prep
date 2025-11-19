ab50fdee002cb0e30956e1f4cd633335add92e49
xSSanDev <xssanass@gmail.com>
2025-11-19 15:10:07 +0100

    ex01

---
+++ C08/ft_boolean.h
@@ -0,0 +1,9 @@
+#ifndef FT_BOOLEAN_H
+#define FT_BOOLEAN_H
+#define SUCCESS 0
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "I have an even number of arguments"
+#define ODD_MSG "I have an odd number of arguments"
+#endif
+
